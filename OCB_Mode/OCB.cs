0616e10629af3b474943a68111e0ad2c38e76ea2
onurkocak <onur.kocak@outlook.com.tr>
2025-08-13 22:02:28 +0300

    Proje dosyası ekle.

---
+++ OCB_Mode/OCB.cs
@@ -0,0 +1,56 @@
+﻿using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Modes;
+using Org.BouncyCastle.Crypto.Parameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCB_Mode
+{
+    class OCB
+    {
+        static readonly int MAC_SIZE_BITS = 128;
+
+        public static byte[] Encrypt(byte[] input, byte[] associatedData, byte[] key, byte[] nonce)
+        {
+            OcbBlockCipher ocb = new OcbBlockCipher(new AesEngine(), new AesEngine());
+            AeadParameters parameters = new AeadParameters(
+                new KeyParameter(key),
+                MAC_SIZE_BITS,
+                nonce,
+                associatedData
+            );
+            ocb.Init(true, parameters);
+            byte[] output = new byte[ocb.GetOutputSize(input.Length)];
+            int length = ocb.ProcessBytes(input, 0, input.Length, output, 0);
+            length += ocb.DoFinal(output, length);
+
+            byte[] result = new byte[length];
+            Array.Copy(output, 0, result, 0, length);
+            return result;
+        }
+        public static byte[] Decrypt(byte[] cipherText, byte[] associatedData, byte[] key, byte[] nonce)
+        {
+            OcbBlockCipher ocb = new OcbBlockCipher(new AesEngine(), new AesEngine());
+            AeadParameters parameters = new AeadParameters(
+                new KeyParameter(key),
+                MAC_SIZE_BITS,
+                nonce,
+                associatedData
+            );
+            ocb.Init(false, parameters);
+            // Çıkış verisi
+            byte[] output = new byte[ocb.GetOutputSize(cipherText.Length)];
+            // Şifre çözme işlemi
+            int length = ocb.ProcessBytes(cipherText, 0, cipherText.Length, output, 0);
+            length += ocb.DoFinal(output, length);
+
+            // Sonucu döndür
+            byte[] result = new byte[length];
+            Array.Copy(output, 0, result, 0, length);
+            return result;
+        }
+    }
+}
---
+++ OCB_Mode/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using OCB_Mode;
+
+try
+{
+    byte[] plainText = Commons.StringToByteArray("0001020304050607");
+    byte[] key = Commons.StringToByteArray("000102030405060708090A0B0C0D0E0F");
+    byte[] nonce = Commons.StringToByteArray("BBAA99887766554433221101");
+    byte[] associatedData = Commons.StringToByteArray("0001020304050607");
+
+    // Şifreleme
+    byte[] cipherText = OCB.Encrypt(plainText, associatedData, key, nonce);
+    Console.WriteLine("Şifrelenmiş Veri: " + Commons.ByteArrayToString(cipherText));
+
+    // Şifre çözme
+    byte[] decryptedText = OCB.Decrypt(cipherText, associatedData, key, nonce);
+    Console.WriteLine("Çözülmüş Veri: " + Commons.ByteArrayToString(decryptedText));
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Hata: " + ex.Message);
+}
